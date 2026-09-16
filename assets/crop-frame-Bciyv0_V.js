7be6623248e05753d5580e30f17765c5c432c9ac
啟揚機電 <chiyang7039.admin@gmail.com>
2026-09-16 03:06:37 +0000

    訪談卡片加播放圖示與看影片按鈕

--- assets/crop-frame-Bciyv0_V.js
+++
@@ -1,1 +0,0 @@
-import{y as e}from"./index-7VGhHt7V.js";var t=e();function n({children:e}){return(0,t.jsxs)(`div`,{className:`crop`,children:[(0,t.jsx)(`span`,{className:`crop-mark crop-tl`}),(0,t.jsx)(`span`,{className:`crop-mark crop-tr`}),(0,t.jsx)(`span`,{className:`crop-mark crop-bl`}),(0,t.jsx)(`span`,{className:`crop-mark crop-br`}),e]})}export{n as t};
